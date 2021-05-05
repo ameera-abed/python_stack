b50a2443c7d11207b8ca4a744fe5e23e7939aa87
ameera <ameeraabedalnaser2021@gmail.com>
2021-05-05 14:13:58 +0300

    update

---
+++ _python/python_fundamentals/hello_world.py
@@ -0,0 +1,10 @@
+print("hello world")
+x="ameera"
+print("hello",x)
+print("hello"+ " "+x)
+z=9
+print("hello",z)
+print("hello"+ " "+str(z))
+food1="banana"
+food2="cake"
+print("i love the "+food1+" "+"and"+" "+food2)
