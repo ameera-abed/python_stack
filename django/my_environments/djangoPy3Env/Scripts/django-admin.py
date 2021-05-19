859211f5d1acdfb626976446ee920531bed3a777
ameera <ameeraabedalnaser2021@gmail.com>
2021-05-19 10:52:30 +0300

    update

---
+++ django/my_environments/djangoPy3Env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ameera\desktop\python_stack\python_stack\django\my_environments\djangopy3env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
