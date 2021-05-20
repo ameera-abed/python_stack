0204e5d6f6612297465f034d439971edc839056a
ameera <ameeraabedalnaser2021@gmail.com>
2021-05-20 11:13:48 +0300

    update

---
+++ django/django_fundamentals/counter/counter_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def root(request):
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
+    context={
+        "num_visits":num_visits,
+    }
+    return render(request,"index.html",context)
